4c6f2338ac1e7fd82de395989947b8b22b26a183
PriyadharshiniKannan-22 <161860353+PriyadharshiniKannan-22@users.noreply.github.com>
2025-08-05 22:49:44 +0530

    Add files via upload
    Program added

---
+++ src/MyJavaApp.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class MyJavaApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String userName = scan.nextLine();
+
+        String welcomeMessage = "Hello, " + userName + "! Welcome to the math quiz.";
+        System.out.println(welcomeMessage);
+
+        String question = "What is 3+2? ";
+        System.out.print(question);
+        String userAnswer = scan.nextLine();
+
+        question = "What is 6/2? ";
+        System.out.print(question);
+        userAnswer = scan.nextLine();
+
+        question = "What is 3*5? ";
+        System.out.print(question);
+        userAnswer = scan.nextLine();
+
+        question = "What is 6-2? ";
+        System.out.print(question);
+        userAnswer = scan.nextLine();
+
+        question = "What is 15/5? ";
+        System.out.print(question);
+        userAnswer = scan.nextLine();
+
+        String farewellMessage = "Thanks for giving the quiz, " + userName + ". You will get your result soon.";
+        System.out.println(farewellMessage);
+
+    }
+}   
